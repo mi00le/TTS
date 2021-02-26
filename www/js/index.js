aab9ef34694b88b5736ffd386a5cf6afc8fb002d
michael <michael.lindfors@hotmail.com>
2021-02-26 23:51:08 +0200

    added PDF support

--- www/js/index.js
+++ www/js/index.js
@@ -1,4 +1,11 @@
 document.addEventListener('deviceready', function () {
+    var pdfjsLib = window['pdfjs-dist/build/pdf'];
+
+    //Add path to pdf.worker
+    pdfjsLib.GlobalWorkerOptions.workerSrc = './pdf.worker.min.js';
+
+
+//Check if speechSynthesis is in window
     if ('speechSynthesis' in window) {
 
         speechSynthesis.onvoiceschanged = function () {
@@ -20,47 +27,75 @@ document.addEventListener('deviceready', function () {
 
         $('#speak').click(function () {
 
-
             if ($('#file').val() == "") {
                 alert("No File Selected!")
             } else {
                 if (!flag) {
                     flag = true;
-                    let file = document.querySelector("#file");
+                    let file = document.querySelector("#file").files[0];
 
-                    var fr = new FileReader();
-                    fr.onload = function () {
+                    if (file.type == "application/pdf") {
+                        let arr = [];
+                        var itemsProcessed = 0;
 
-                        var text = fr.result;
-                        
-                        
-                        
-                        var msg = new SpeechSynthesisUtterance();
-                        var voices = window.speechSynthesis.getVoices();
-                        msg.voice = voices[$('#voices').val()];
-                        msg.rate = $('#rate').val() / 10;
-                        msg.pitch = $('#pitch').val();
-                        msg.text = text;
-                        
-                        $('.output').html(text)
+                        //Convert file to buffer
+                        var bufferPromise = file.arrayBuffer();
+                        bufferPromise.then((buffer) => {
 
+                            // pdfjsLib.getDocument only accepts .data == buffer
+                            var loadingTask = pdfjsLib.getDocument(buffer);
 
-                        msg.onend = function (e) {
-                            console.log('Finished in ' + event.elapsedTime + ' seconds.');
-                            flag = false;
-                        };
-                        speechSynthesis.speak(msg);
+                            loadingTask.promise.then(function (pdf) {
+                                var numPages = pdf.numPages;
 
-                        let r = setInterval(() => {
-                            console.log(speechSynthesis.speaking);
-                            if (!speechSynthesis.speaking) {
-                                clearInterval(r);
-                            } else {
-                                speechSynthesis.resume();
-                            }
-                        }, 14000);
+                                for (let i = 1; i <= numPages; i++) {
+                                    itemsProcessed++;
+                                    pdf.getPage(i).then(function (page) {
+                                        page.getTextContent().then(function (textContent) {
+                                            arr.push(textContent.items)
+                                            if (i >= numPages) {
+                                                read_out_pdf(arr)
+                                            }
+                                        });
+                                    });
+                                }
+                            });
+                        })
+                    } else {
+
+                        var fr = new FileReader();
+                        fr.onload = function () {
+
+                            var text = fr.result;
+
+                            //initialize SpeechSynthesisUtterance
+                            var msg = new SpeechSynthesisUtterance();
+
+                            var voices = window.speechSynthesis.getVoices();
+                            msg.voice = voices[$('#voices').val()];
+                            msg.rate = $('#rate').val() / 10;
+                            msg.pitch = $('#pitch').val();
+                            msg.text = text;
+
+                            $('.output').html(text)
+
+                            msg.onend = function (e) {
+                                console.log('Finished in ' + event.elapsedTime + ' seconds.');
+                                flag = false;
+                            };
+                            speechSynthesis.speak(msg);
+
+                            let r = setInterval(() => {
+                                console.log(speechSynthesis.speaking);
+                                if (!speechSynthesis.speaking) {
+                                    clearInterval(r);
+                                } else {
+                                    speechSynthesis.resume();
+                                }
+                            }, 14000);
+                        }
+                        fr.readAsText(file.files[0]);
                     }
-                    fr.readAsText(file.files[0]);
                 }
 
                 if (window.speechSynthesis.paused) {
@@ -69,16 +104,56 @@ document.addEventListener('deviceready', function () {
             }
         })
 
+        let arr = [];
+        function read_out_pdf(items) {
+            arr = [];
+
+            //flatten array
+            let flattenArr = items.flat()
+
+            //map out str from array and join to string
+            var text = flattenArr.map(o => o.str).join('');
+
+            //initialize SpeechSynthesisUtterance
+            var msg = new SpeechSynthesisUtterance();
+            var voices = window.speechSynthesis.getVoices();
+            msg.voice = voices[$('#voices').val()];
+            msg.rate = $('#rate').val() / 10;
+            msg.pitch = $('#pitch').val();
+            msg.text = text;
+
+            $('.output').html(text)
+
+            msg.onend = function (e) {
+                console.log('Finished in ' + event.elapsedTime + ' seconds.');
+                flag = false;
+            };
+
+            //Invoke speech function
+            speechSynthesis.speak(msg);
+
+            //check if no sound
+            let r = setInterval(() => {
+                console.log(speechSynthesis.speaking);
+                if (!speechSynthesis.speaking) {
+                    clearInterval(r);
+                } else {
+                    speechSynthesis.resume();
+                }
+            }, 14000);
+
+        }
+
+
         $('#pause').click(() => {
             if (speechSynthesis.speaking && !speechSynthesis.paused) { /* pause narration */
                 speechSynthesis.pause();
             }
-            // flag = true;
         })
 
 
         $('#cancel').click(() => {
-            if (speechSynthesis.speaking) { /* stop narration */
+            if (speechSynthesis.speaking) {
                 /* for safari */
                 flag = false;
                 speechSynthesis.cancel();
@@ -90,11 +165,11 @@ document.addEventListener('deviceready', function () {
             $('#file').click();
         })
 
-        $('#rate').on('input',()=>{
-            $('#rate_label').html($('#rate').val())
+        $('#rate').on('input', () => {
+            $('#rate_label').html(`Speed: ${$('#rate').val()}`)
         })
-        $('#pitch').on('input',()=>{
-            $('#pitch_label').html($('#pitch').val())
+        $('#pitch').on('input', () => {
+            $('#pitch_label').html(`Pitch: ${$('#pitch').val()}`)
         })
     }
     else {
